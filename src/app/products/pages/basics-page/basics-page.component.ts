b11d9fddbcf9f1c4c7fc27637298a5a78238f9ca
Daniela Liendo <daniela@ualabee.com>
2023-05-18 16:33:56 +0200

    basics page component was created

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-basics-page',
+  templateUrl: './basics-page.component.html',
+  styleUrls: ['./basics-page.component.css']
+})
+export class BasicsPageComponent {
+
+}
