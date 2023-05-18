664b3162dfd99366772fad8068286169d2901868
Daniela Liendo <daniela@ualabee.com>
2023-05-18 16:35:26 +0200

    menu component was added

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+  ]
+})
+
+export class MenuComponent implements OnInit {
+
+  public items: MenuItem[] = []
+
+  ngOnInit(): void {
+
+    this.items = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y fechas',
+            icon: 'pi pi-align-left',
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar'
+          },
+          {
+            label: 'Poco comúnes',
+            icon: 'pi pi-globe'
+          }
+        ]
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro elemento',
+            icon: 'pi pi-cog',
+          }
+        ]
+      }
+    ];
+
+  }
+
+}
