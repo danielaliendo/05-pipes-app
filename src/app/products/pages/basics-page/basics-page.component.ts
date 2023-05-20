e9107f04f72483da1cbfa7e69280ef511b9ed4c0
Daniela Liendo <daniela@ualabee.com>
2023-05-20 13:33:19 +0200

    basics component page was added

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './basics-page.component.html',
   styleUrls: ['./basics-page.component.css']
 })
+
 export class BasicsPageComponent {
+
+  public nameLowercase: string = 'daniela liendo'
+  public nameUppercase: string = 'DANIELA LIENDO'
+  public nameCamelcase: string = 'DaNiElA lIeNdO'
+
+  public date: Date = new Date();
 
 }
