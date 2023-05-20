351d7c791740908c66aa2d650594bd7b2e5ab5d8
Daniela Liendo <daniela@ualabee.com>
2023-05-20 13:35:33 +0200

    order page component and custom pipes were added to declarations list and prime ng module was imported inside products module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,3 +1,4 @@
+import { CanFlyCasePipe } from './pipes/can-fly.pipe';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -5,17 +6,28 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { OrderPageComponent } from './pages/order-page/order-page.component';
 
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { SortByPipe } from './pipes/sort-by.pipe';
 
 @NgModule({
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent
+    UncommonPageComponent,
+    OrderPageComponent,
+
+    // Pipes
+    ToggleCasePipe,
+    CanFlyCasePipe,
+    SortByPipe
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    PrimeNgModule
   ]
 })
 export class ProductsModule { }
