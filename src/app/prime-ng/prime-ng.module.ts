74ae65c0f3e188b2e43ac48f3159da114ce7cce5
Daniela Liendo <daniela@ualabee.com>
2023-05-20 13:32:32 +0200

    prime ng modules were added

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,10 +1,24 @@
+import { ButtonModule } from 'primeng/button';
 import { NgModule } from '@angular/core';
-import {MenubarModule} from 'primeng/menubar';
+import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+import { SplitButtonModule } from 'primeng/splitbutton';
 
 @NgModule({
   exports: [
-    MenubarModule
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    MenubarModule,
+    PanelModule,
+    TableModule,
+    ToolbarModule,
+    SplitButtonModule
   ]
 })
 
-export class PrimeNgModule {}
+export class PrimeNgModule { }
