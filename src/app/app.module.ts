5080f5322afea02400cad98298950daeed62d868
Daniela Liendo <daniela@ualabee.com>
2023-05-20 13:32:04 +0200

    locale languages were added to providers list inside app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -6,6 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
+
+// Locale settings
+import localeEs419 from '@angular/common/locales/es-419';
+import localeFrCA from '@angular/common/locales/fr-CA';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs419);
+registerLocaleData(localeFrCA);
 
 @NgModule({
   declarations: [
@@ -18,7 +26,11 @@ import { SharedModule } from './shared/shared.module';
     PrimeNgModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'en'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 
